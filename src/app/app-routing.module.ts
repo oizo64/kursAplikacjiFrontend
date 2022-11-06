f60e612ea96f8dad763468b8f3656517ce529c50
Piotr <oizo64@gmail.com>
2022-11-06 15:33:24 +0100

    Fullpage Layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
+import { FullpageComponent } from './layouts/fullpage/fullpage.component';
 import { HomeComponent } from './modules/home/home.component';
+import { LoginComponent } from './modules/login/login.component';
 import { ProductComponent } from './modules/product/product.component';
 
-const routes: Routes = [{
-  path:'', component: DefaultComponent, children: [
-    {
-      path:'', component: HomeComponent
-    },
-    {
-      path:'products', component: ProductComponent
-    }
-  ]
-}];
+const routes: Routes = [
+  {
+    path: '', component: DefaultComponent, children: [
+      { path: '', component: HomeComponent },
+      { path: 'products', component: ProductComponent }
+    ]
+  },  {
+    path: '', component: FullpageComponent, children: [
+      { path: 'login', component: LoginComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
