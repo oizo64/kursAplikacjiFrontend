6a52f2f7f5b7d6d1c3405fe2e1c96754bbf9d102
Piotr <oizo64@gmail.com>
2022-11-06 15:21:17 +0100

    Domyślny Layout aplikacji

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RouterModule } from '@angular/router';
+import { FlexModule } from '@angular/flex-layout';
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    SidebarComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FlexModule
+  ],
+  exports: [
+    FooterComponent,
+    SidebarComponent,
+    HeaderComponent
+  ]
+})
+export class SharedModule { }
